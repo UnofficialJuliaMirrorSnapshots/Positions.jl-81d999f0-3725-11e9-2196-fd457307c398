f18ba33e563b5a0848657cd9dee186199f3c3ecb
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-08 01:28:48 +0000

    Snapshot of branch master taken on 2019-04-07T21:28:46.929-04:00 from "https://github.com/UnofficialJuliaMirror/Positions.jl-81d999f0-3725-11e9-2196-fd457307c398"

---
+++ src/Positions.jl
@@ -0,0 +1,43 @@
+module Positions
+
+using Reexport
+@reexport using FinancialInstruments, FixedPointDecimals
+export Position
+
+abstract type AbstractPosition end
+
+struct Position{F<:FinancialInstrument,A<:Real} <: AbstractPosition
+    amount::A
+end
+Position(p,a) = Position{typeof(p),typeof(a)}(a)
+Position(c::Cash{C},a) where C = Position{Cash{C},FixedDecimal{Int,Currencies.unit(C())}}(FixedDecimal{Int,Currencies.unit(C())}(a))
+
+Base.promote_rule(::Type{Position{F,A1}}, ::Type{Position{F,A2}}) where {F,A1,A2} = Position{F,promote_type(A1,A2)}
+Base.convert(::Type{Position{F,A}}, x::Position{F,A}) where {F,A} = x
+Base.convert(::Type{Position{F,A}}, x::Position{F,<:Real}) where {F,A} = Position{F,A}(convert(A,x.amount))
+
+Base.:+(p1::Position{F,A},p2::Position{F,A}) where {F,A} = Position{F,A}(p1.amount+p2.amount)
+Base.:+(p1::AbstractPosition,p2::AbstractPosition) = +(promote(p1,p2)...)
+
+Base.:-(p1::Position{F,A},p2::Position{F,A}) where {F,A} = Position{F,A}(p1.amount-p2.amount)
+Base.:-(p1::AbstractPosition,p2::AbstractPosition) = -(promote(p1,p2)...)
+
+Base.:/(p1::Position{F,A},p2::Position{F,A}) where {F,A} = p1.amount/p2.amount
+Base.:/(p1::AbstractPosition,p2::AbstractPosition) = /(promote(p1,p2)...)
+
+# TODO: Should scalar multiplication and division respect the Position type or do normal promotion?
+
+# Base.:/(p::Position{F,A},k::R) where {F,A,R<:Real} = Position{F,promote_type(A,R)}(p.amount/k)
+Base.:/(p::Position{F,A},k::R) where {F,A,R<:Real} = Position{F,A}(p.amount/k)
+
+# Base.:*(k::R,p::Position{F,A}) where {F,A,R<:Real} = Position{F,promote_type(A,R)}(p.amount*k)
+Base.:*(k::R,p::Position{F,A}) where {F,A,R<:Real} = Position{F,A}(p.amount*k)
+Base.:*(p::Position{F,A},k::R) where {F,A,R<:Real} = k*p
+
+Base.show(io::IO,c::Position{Cash{C}}) where C = print(io,c.amount," ",C())
+Base.show(io::IO,::MIME"text/plain",c::Position{Cash{C}}) where C = print(io,c.amount," ",C())
+
+Base.zero(::Type{Position{F,A}}) where {F,A} = Position{F,A}(zero(A))
+Base.one(::Type{Position{F,A}}) where {F,A} = Position{F,A}(one(A))
+
+end # module
